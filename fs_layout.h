33c1045ac540ed805fab3ddeebe5a525c544b7bf
g4197 <cfhmgh@163.com>
2022-05-04 11:39:07 +0800

    Add idx3

--- fs_layout.h
+++ fs_layout.h
@@ -68,6 +68,10 @@ struct IndirectIndex2 {
     blk_id_t indirect1[kBlockSize / sizeof(blk_id_t)];
 };
 
+struct IndirectIndex3 {
+    blk_id_t indirect2[kBlockSize / sizeof(blk_id_t)];
+};
+
 /* Same to DiskInode in rCore */
 struct DiskInode {
     /* Bytes for dir/file, use total_blocks to get block num. */
@@ -87,6 +91,7 @@ struct DiskInode {
     blk_id_t direct[kInodeDirectCnt];
     blk_id_t indirect1;
     blk_id_t indirect2;
+    blk_id_t indirect3;
     DiskInodeType type;
 
     /* Metadata (create time etc.) should be updated. */
